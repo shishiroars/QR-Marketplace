88e296a7f6d63d550a2ea869e4fd8af26f0afd42
Shishir Shrivastava <shishir.sep@gmail.com>
2022-01-18 13:48:15 +0530

    Initial commit
    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-on:click="goToProductsPage('product1')">Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+      <li>Product 4</li>
+      <li>Product 5</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'QR Code App'
+    }
+  },
+  methods: {
+    goToProductsPage: function (message) {
+      this.$router.push({ name: 'ProdctDetails', params: { productId: message } })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/components/ProdctDetails.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <div>Product 1</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProdctDetails',
+  data () {
+    return {
+      msg: 'Product Details'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+    font-weight: normal;
+    }
+    ul {
+    list-style-type: none;
+    padding: 0;
+    }
+    li {
+    display: inline-block;
+    margin: 0 10px;
+    }
+    a {
+    color: #42b983;
+    }
+</style>
